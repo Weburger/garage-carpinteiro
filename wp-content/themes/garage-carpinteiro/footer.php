09018775f6a67709ed30c4d1295af9cdf0e689dd
Michael <gaelle.balois@gmail.com>
2024-05-14 15:34:08 +0200

    header et banniere

--- wp-content/themes/garage-carpinteiro/footer.php
+++ wp-content/themes/garage-carpinteiro/footer.php
@@ -1,1 +1,44 @@
-<?php
+<?php ?>
+
+<footer id="footer">
+    <div class="footer-widgets">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <?php if ( is_active_sidebar( 'footer-widget-1' ) ) : ?>
+                        <div class="footer-widget">
+                            <?php dynamic_sidebar( 'footer-widget-1' ); ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <div class="col-md-4">
+                    <?php if ( is_active_sidebar( 'footer-widget-2' ) ) : ?>
+                        <div class="footer-widget">
+                            <?php dynamic_sidebar( 'footer-widget-2' ); ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <div class="col-md-4">
+                    <?php if ( is_active_sidebar( 'footer-widget-3' ) ) : ?>
+                        <div class="footer-widget">
+                            <?php dynamic_sidebar( 'footer-widget-3' ); ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
+
--- wp-content/themes/garage-carpinteiro/header.php
+++ wp-content/themes/garage-carpinteiro/header.php
@@ -1,1 +1,29 @@
-<?php
+<?php ?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header id="header">
+        <nav id="main-navigation">
+            <div class="logo-header"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">GARAGE CARPINTEIRO</a></div>
+            <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'primary',
+                    'container' => false,
+                    'menu_id' => 'main-menu',
+                    'menu_class' => 'menu'
+                ));
+            ?>
+        </nav>
+    </header>
