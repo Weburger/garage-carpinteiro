22b905d95036008c68e4d17fb3ba1cccf69cca28
aserlite <arthurcuvillon691@gmail.com>
2024-06-03 09:55:27 +0200

    critair def

--- wp-content/themes/garage-carpinteiro/functions.php
+++ wp-content/themes/garage-carpinteiro/functions.php
@@ -290,12 +290,10 @@ function voiture_meta_box_callback($post) {
         echo "-- Choisir une option --";
     }
     echo'</option>
-        <option value="0">0</option>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
         <option value="4">4</option>
-        <option value="5">5</option>
     </select></br></br>';
     /*
     echo '<label for="puissance_fiscale">' . __('Puissance fiscale (cv) ') . '</label>';
